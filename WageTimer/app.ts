255220ece804ace5468f96383800019a72d97055
Seth Hendrick <seth@shendrick.net>
2017-02-12 16:39:24 -0500

    Add Licensing.

--- WageTimer/app.ts
+++ WageTimer/app.ts
@@ -1,4 +1,10 @@
-﻿class Greeter {
+﻿
+//          Copyright Seth Hendrick 2017.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+class Greeter {
     element: HTMLElement;
     span: HTMLElement;
     timerToken: number;
