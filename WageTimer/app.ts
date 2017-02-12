6a648832f94cc362da77b4be92cd466f84099ed0
Seth Hendrick <seth@shendrick.net>
2017-02-12 17:54:51 -0500

    UI is mostly working...
    need to figure out scrolling (oh god why) and how to start/stop a timer in TypeScript.

--- WageTimer/app.ts
+++ WageTimer/app.ts
@@ -4,31 +4,68 @@
 //    (See accompanying file LICENSE_1_0.txt or copy at
 //          http://www.boost.org/LICENSE_1_0.txt)
 
-class Greeter {
-    element: HTMLElement;
-    span: HTMLElement;
-    timerToken: number;
+class WageTimer {
 
-    constructor(element: HTMLElement) {
-        this.element = element;
-        this.element.innerHTML += "The time is: ";
-        this.span = document.createElement('span');
-        this.element.appendChild(this.span);
-        this.span.innerText = new Date().toUTCString();
+    // ---------------- Fields ----------------
+
+    startButton: HTMLButtonElement;
+    resetButton: HTMLButtonElement;
+    amountText: HTMLSpanElement;
+    wageText: HTMLTextAreaElement;
+
+    currentWage: Number;
+
+    amount: Number;
+
+    constructor(startButton: HTMLButtonElement, resetButton: HTMLButtonElement, amountText: HTMLSpanElement, wageText: HTMLTextAreaElement) {
+        this.startButton = startButton;
+        this.resetButton = resetButton;
+        this.amountText = amountText;
+        this.wageText = wageText;
+        this.amount = 0.0;
+        this.currentWage = 0.0;
+        this.wageText.style.borderColor = "black";
     }
 
-    start() {
-        this.timerToken = setInterval(() => this.span.innerHTML = new Date().toUTCString(), 500);
+    startButtonClicked() {
+        if (this.validateWage()) {
+            this.wageText.style.borderColor = "black";
+            this.wageText.disabled = true;
+        }
+        else {
+            this.wageText.style.borderColor = "red";
+            this.wageText.style.borderWidth = "5px";
+        }
     }
 
-    stop() {
-        clearTimeout(this.timerToken);
+    resetButtonClicked() {
+        this.amount = 0.0;
+        this.updateAmountText();
+        this.wageText.disabled = false;
     }
 
+    updateAmountText() {
+        this.amountText.innerHTML = "$" + this.amount.toFixed(2);
+    }
+
+    validateWage(): boolean {
+        var wage = Number(this.wageText.value);
+        if (isNaN(wage)) {
+            return false;
+        }
+
+        this.currentWage = wage;
+        return true;
+    }
 }
 
+var timer;
+
 window.onload = () => {
-    var el = document.getElementById('content');
-    var greeter = new Greeter(el);
-    greeter.start();
+    var startButton = <HTMLButtonElement>document.getElementById("startbutton");
+    var resetButton = <HTMLButtonElement>document.getElementById("resetbutton");
+    var amountEarned = <HTMLSpanElement>document.getElementById("amountearned");
+    var wageText = <HTMLTextAreaElement>document.getElementById("wageinput");
+
+    timer = new WageTimer(startButton, resetButton, amountEarned, wageText);
 };
